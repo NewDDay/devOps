7d43e6ee104babdfc6cc434493283dc8d9cb2c4e
Dmitry <rosenrot@zyryanovdmm.fvds.ru>
2021-12-12 12:59:13 +0300

    Add removing julia directory after making docker image

--- tgbot/mkDockerImage.sh
+++ tgbot/mkDockerImage.sh
@@ -3,5 +3,6 @@
 curl -O https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.0-linux-x86_64.tar.gz
 tar -xvf julia-1.7.0-linux-x86_64.tar.gz
 rm julia-1.7.0-linux-x86_64.tar.gz
-docker build -t tgbot:v0.1 .
-# docker run -d -p 2000:443 tgbot:v0.1 # If you want to run image
+docker build -t tgbot:v0.2 .
+rm -r julia-1.7.0
+docker run -d -p 2000:443 tgbot:v0.2 # If you want to run image
